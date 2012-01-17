f85929f57748026360c12f5d497d8e5e9f850bbd
hiroshi sendai <hiroshi.sendai@kek.jp>
2012-01-17 10:39:32 +0900

    Firefox has send SYN packets every time we click the buttons.  This caused huge memory comsumption and we could not get any feedback when we click the buttons.  This commit fixes #71.

--- www/js/daq.js
+++ www/js/daq.js
@@ -4,7 +4,8 @@
  * @brief methods for DAQ Panel
  * @date 25-May-2011
  * @author Yoshiji Yasu
- *        
+ * @author Hiroshi Sendai
+ * @modified 13-January-2012
  *
  * Copyright (C) 2008-2011
  *     High Energy Accelerator Research Organization (KEK), Japan.
@@ -13,26 +14,27 @@
  */
 
   /**
-   * repeatGetState1 "getstate(./daq.py/Log')" wtih the specified count, times.
+   *  setReflesh();
+   *  clearReflesh();
+   *    calls "getstate(./daq.py/Log')"
    */
-  function repeatGetState(times) {
-    var i = 0;
-    setInterval((
-      function() {
-        getstate('./daq.py/Log');
-        i++;
-        if ((times != 0) && (i == times)) {
-          clearInterval();
-        }
-      }
-    ), 2000); // 2 sec.
+  var timeID;
+  function clearReflesh() {
+    clearInterval(timeID);
+  }
+  function timerFunc() {
+    getstate('./daq.py/Log');
+  }
+  function setReflesh() {
+    clearInterval(timeID);
+    timeID = setInterval(timerFunc, 2000); // 2 sec.
   }
 
   /**
    * change to Configured state
    */
   function change2Configured() {
-    inputs = document.getElementsByTagName("input");
+    var inputs = document.getElementsByTagName("input");
     inputs[3].src = "../parts/configurationbuttonlight.jpg";
     inputs[5].src = "../parts/beginbutton.jpg";
     inputs[7].src = "../parts/endbuttonlight.jpg";
@@ -45,14 +47,14 @@
     $("unconfigId").disabled = false;
     $("pauseId").disabled = true;
     $("restartId").disabled = true;
-    repeatGetState(5);
+    inputs = null;
   }
 
   /**
    * change to Running state
    */
   function change2Running() {
-    inputs = document.getElementsByTagName("input");
+    var inputs = document.getElementsByTagName("input");
     inputs[3].src = "../parts/configurationbuttonlight.jpg";
     inputs[5].src = "../parts/beginbuttonlight.jpg";
     inputs[7].src = "../parts/endbutton.jpg";
@@ -65,14 +67,14 @@
     $("unconfigId").disabled = true;
     $("pauseId").disabled = false;
     $("restartId").disabled = true;
-    repeatGetState(0);
+    inputs = null;
   }
 
   /**
    * change to Paused state
    */
   function change2Paused() {
-    inputs = document.getElementsByTagName("input");
+    var inputs = document.getElementsByTagName("input");
     inputs[3].src = "../parts/configurationbuttonlight.jpg";
     inputs[5].src = "../parts/beginbuttonlight.jpg";
     inputs[7].src = "../parts/endbuttonlight.jpg";
@@ -85,14 +87,14 @@
     $("unconfigId").disabled = true;
     $("pauseId").disabled = true;
     $("restartId").disabled = false;
-    repeatGetState(5);
+    inputs = null;
   }
 
   /**
    * change to Loaded state
    */
   function change2Loaded() {
-    inputs = document.getElementsByTagName("input");
+    var inputs = document.getElementsByTagName("input");
     inputs[3].src = "../parts/configurationbutton.jpg";
     inputs[5].src = "../parts/beginbuttonlight.jpg";
     inputs[7].src = "../parts/endbuttonlight.jpg";
@@ -105,64 +107,82 @@
     $("unconfigId").disabled = true;
     $("pauseId").disabled = true;
     $("restartId").disabled = true;
-    repeatGetState(5);
+    inputs = null;
   }
 
   /**
    * get state from the url
    */
-  function getstate(url) {
-    var msec = (new Date()).getTime();
-    new Ajax.Request(url, {
-      method: "get",
-      parameters: "cache="+msec,
-      onCreate:function(httpObj){
-//        alert(httpObj+" was got.");
-      },
-      onSuccess:function(httpObj){
+  function onSuccessFunc(httpObj) {
 //        alert("OK: "+ httpObj.responseText);
         var res = httpObj.responseXML;
 //        alert("OK: "+ httpObj.responseXML);
-	var compNames = res.getElementsByTagName('compName');
-        var status = res.getElementsByTagName('status');
-        $("commandStatus").innerHTML = status[0].firstChild.nodeValue;
-        var states = res.getElementsByTagName('state');
-        var currentstatus = states[0].firstChild.nodeValue;
-        var compStatus = res.getElementsByTagName('compStatus');
-        var eventnums = res.getElementsByTagName('eventNum');
-        var length = states.length;
-        text="";
-        var len = compNames.length;
-        for(i=0; i<length;i+=len) {
-          text +="<table><tr><th></th><th>State</th><th>Status</th><th>Event Counts</th></tr>";
-          for(j = 0; j< len; j++) {
-            text +="<tr><td>";
-            text += compNames[i+j].firstChild.nodeValue;
-            text += "</td><td class='compState'>";
-            text += states[i+j].firstChild.nodeValue;
-            text += "</td><td class='compStatus'>";
-            text +=compStatus[i+j].firstChild.nodeValue;
-            text +="</td><td class='eventCount'>";
-            text += eventnums[i+j].firstChild.nodeValue;
-            text +="</td></tr>";
-          }
-          text +="</table>";
-          //alert(text);
-        }
-        //alert(text);
-        $("DAQStatus").innerHTML = text;
-
-        if(currentstate == "LOADED")
-          change2Loaded1();
-        else if (currentstate == "CONFIGURED")
-          change2Configured1();
-        else if (currentstat == "RUNNING")
-          change2Running1();
-        else if (currentstat == "PAUSED")
-          change2Paused1();
-      },
-      onFailure:function(httpObj){
-//        alert("Bad: "+ httpObj.responseText);
+    var compNames = res.getElementsByTagName('compName');
+    var status = res.getElementsByTagName('status');
+    var value = status[0].firstChild.nodeValue;
+    $("commandStatus").innerHTML = value;
+    value = null;
+    var states = res.getElementsByTagName('state');
+    var currentstate = states[0].firstChild.nodeValue;
+    var compStatus = res.getElementsByTagName('compStatus');
+    var eventnums = res.getElementsByTagName('eventNum');
+    var length = states.length;
+    var text="";
+    var len = compNames.length;
+    for(i=0; i<length;i+=len) {
+      text +="<table><tr><th></th><th>State</th><th>Status</th><th>Total Byte Counts</th></tr>";
+      for(j = 0; j< len; j++) {
+        text +="<tr><td>";
+        text += compNames[i+j].firstChild.nodeValue;
+        text += "</td><td class='compState'>";
+        text += states[i+j].firstChild.nodeValue;
+        text += "</td><td class='compStatus'>";
+        text +=compStatus[i+j].firstChild.nodeValue;
+        text +="</td><td class='eventCount'>";
+        text += eventnums[i+j].firstChild.nodeValue;
+        text +="</td></tr>";
       }
+      text +="</table>";
+      //alert(text);
+    }
+    res = null;
+    compNames = null;
+    status = null;
+    states = null;
+    compStatus = null;
+    eventnums = null;
+    //alert(text);
+    $("DAQStatus").innerHTML = text;
+    text = null;
+
+    if(currentstate == "LOADED")
+      change2Loaded();
+    else if (currentstate == "CONFIGURED")
+      change2Configured();
+    else if (currentstate == "RUNNING")
+      change2Running();
+    else if (currentstate == "PAUSED")
+      change2Paused();
+    currentstate = null;
+  }
+
+  function onCreateFunc(httpObj) {
+//        alert(httpObj+" was got.");
+  }
+
+  function onFailureFunc(httpObj){
+//        alert("Bad: "+ httpObj.responseText);
+  }
+
+  function getstate(url) {
+    var date = new Date();
+    var params = "cache" + date.getTime();
+    var myAjax = new Ajax.Request(url, {
+      method: "get",
+      parameters: params,
+      onCreate: onCreateFunc,
+      onSuccess: onSuccessFunc,
+      onFailure: onFailureFunc
     });
   }
+
