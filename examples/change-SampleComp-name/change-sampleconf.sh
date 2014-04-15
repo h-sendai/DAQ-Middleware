0fd41e3334d1dc9e6e8d0fd0c1dac1d750e90c71
hiroshi sendai <hiroshi.sendai@kek.jp>
2014-04-15 14:34:29 +0900

    Add scripts to rewrite sample config.xml for new DAQ-Component name.

---
+++ examples/change-SampleComp-name/change-sampleconf.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# exit if error occured.
+set -e
+
+old_name_camel_case=Sample
+new_name_camel_case=RawData
+
+# You don't have to rewrite following lines
+
+old_name_lower_case=$(echo $old_name_camel_case | tr '[A-Z]' '[a-z]')
+old_name_upper_case=$(echo $old_name_camel_case | tr '[a-z]' '[A-Z]')
+new_name_lower_case=$(echo $new_name_camel_case | tr '[A-Z]' '[a-z]')
+new_name_upper_case=$(echo $new_name_camel_case | tr '[a-z]' '[A-Z]')
+
+for i in sample.xml 4comps.xml; do
+    if [ -f $i ]; then
+        sed -i.bak \
+        -e "s/${old_name_lower_case}/${new_name_lower_case}/g" \
+        -e "s/${old_name_camel_case}/${new_name_camel_case}/g" \
+        -e "s/${old_name_upper_case}/${new_name_upper_case}/g" \
+        $i
+    fi
+done
+
+rm -f *.bak
