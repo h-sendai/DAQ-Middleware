8dea441cc877e7ac83e5e194abef892ea35acccd
hiroshi sendai <hiroshi.sendai@kek.jp>
2015-02-26 13:01:31 +0900

    Fix comment in run.py (we don't use pgrep(1) anymore).

--- bin/run.py
+++ bin/run.py
@@ -360,8 +360,9 @@ def validateConfigFile(confFile, schemaFile):
 def kill_proc_exact(proc_name, sleep_sec = 1, max_retry = 60):
     """
     Send SIGTERM signal to proc_name.  Information of process id
-    will be get from pgrep(1) output.  Process lookup will be done
-    based on exact match of the proc_name.  For example,
+    will be get from get_pids_exact() that use MyProcUtil.
+    Process lookup will be done based on exact match of the proc_name.
+    For example,
     kill_proc_exact('SomeComp') will send SIGTERM to
 
     /usr/local/bin/SomeComp -f rtc.conf
