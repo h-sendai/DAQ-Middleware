918676dbdc7abce0808ce9311306aa3b3749f440
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2020-04-21 08:27:01 +0900

    Update Distributions.mk for python information.

--- Distributions.mk
+++ Distributions.mk
@@ -12,6 +12,8 @@ MAJOR_VERSION = $(shell echo $(VERSION) | sed -e 's/\..*//')
 # default vars
 USE_MOD_PYTHON = 0
 USE_MOD_WSGI   = 0
+USE_PYTHON2    = 0
+USE_PYTHON3    = 0
 
 ########## Scientific Linux 5.0 - 5.7 ##########
 # Scientific Linux older than or equal to 5.7 returns ScientificSL 
@@ -29,12 +31,21 @@ WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/httpd/conf.d
 ifeq ($(strip $(MAJOR_VERSION)),5)
 USE_MOD_PYTHON    = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),6)
 USE_MOD_WSGI      = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),7)
 USE_MOD_WSGI      = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 endif
 
@@ -44,9 +55,15 @@ WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/httpd/conf.d
 ifeq ($(strip $(MAJOR_VERSION)),5)
 USE_MOD_PYTHON    = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),6)
 USE_MOD_WSGI      = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 endif
 # CERN CentOS 7 is like a CentOS 7
@@ -57,17 +74,26 @@ WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/httpd/conf.d
 ifeq ($(strip $(MAJOR_VERSION)),5)
 USE_MOD_PYTHON    = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
+PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),6)
 USE_MOD_WSGI      = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
 PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),7)
 USE_MOD_WSGI      = 1
+USE_PYTHON2       = 1
+PYTHON_EXEC_FILE  = python2
 PYTHON_CONFIG     = python2-config
 endif
 ifeq ($(strip $(MAJOR_VERSION)),8)
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python2
 PYTHON_CONFIG     = python3-config
 endif
 endif
@@ -77,6 +103,9 @@ ifeq ($(strip $(OS)),Fedora)
 WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/httpd/conf.d
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python3
+PYTHON_CONFIG     = python3-config
 endif
 
 ########## Debian ##########
@@ -84,6 +113,9 @@ ifeq ($(strip $(OS)),Debian)
 WWW_DOCUMENT_ROOT = /var/www
 HTTPD_CONF_DIR    = /etc/apache2/conf.d
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python3
+PYTHON_CONFIG     = python3-config
 endif
 
 ########## Ubuntu ##########
@@ -91,6 +123,9 @@ ifeq ($(strip $(OS)),Ubuntu)
 WWW_DOCUMENT_ROOT = /var/www
 HTTPD_CONF_DIR    = /etc/apache2/conf.d
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python3
+PYTHON_CONFIG     = python3-config
 endif
 
 ########## ArchLinux  ##########
@@ -98,6 +133,9 @@ ifeq ($(strip $(OS)),archlinux)
 WWW_DOCUMENT_ROOT = /srv/http
 HTTPD_CONF_DIR    = /etc/httpd/conf/extra
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python
+PYTHON_CONFIG     = python-config
 endif
 
 ########## ArchLinux (lsb_release -si output changed) ##########
@@ -105,6 +143,9 @@ ifeq ($(strip $(OS)),arch)
 WWW_DOCUMENT_ROOT = /srv/http
 HTTPD_CONF_DIR    = /etc/httpd/conf/extra
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python
+PYTHON_CONFIG     = python-config
 endif
 
 ########## Raspbian ##########
@@ -112,6 +153,9 @@ ifeq ($(strip $(OS)),Raspbian)
 WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/apache2/conf-available
 USE_MOD_WSGI      = 1
+USE_PYTHON3       = 1
+PYTHON_EXEC_FILE  = python3
+PYTHON_CONFIG     = python3-config
 endif
 
 ########## Unknown ##########
