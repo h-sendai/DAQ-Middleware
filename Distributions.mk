a0e08082a0049a41e7ca3823757ffc4df9b1e14f
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2021-05-17 12:31:13 +0900

    - indent in Distributions.mk - If CentOS 7 is 7.7 or later use python3

--- Distributions.mk
+++ Distributions.mk
@@ -8,6 +8,7 @@ OS            = $(shell lsb_release -si)
 VERSION       = $(shell lsb_release -sr)
 ARCH          = $(shell uname -m)
 MAJOR_VERSION = $(shell echo $(VERSION) | sed -e 's/\..*//')
+MINOR_VERSION = $(shell echo $(VERSION) | awk -F'.' '{print $$2}')
 
 # default vars
 USE_MOD_PYTHON = 0
@@ -16,55 +17,61 @@ USE_PYTHON2    = 0
 USE_PYTHON3    = 0
 
 ########## Scientific Linux 5.0 - 5.7 ##########
-# Scientific Linux older than or equal to 5.7 returns ScientificSL 
+# Scientific Linux older than or equal to 5.7 returns ScientificSL
 # Scientific Linux newer than or equal to 5.8 returns Scientific
 
 ifeq ($(strip $(OS)),ScientificSL)
-WWW_DOCUMENT_ROOT = /var/www/html
-HTTPD_CONF_DIR    = /etc/httpd/conf.d
-USE_MOD_PYTHON    = 1
+    WWW_DOCUMENT_ROOT = /var/www/html
+    HTTPD_CONF_DIR    = /etc/httpd/conf.d
+    USE_MOD_PYTHON    = 1
 endif
 
 ########## Scientific Linux 5.8 - , 7.x ##########
 ifeq ($(strip $(OS)),Scientific)
-WWW_DOCUMENT_ROOT = /var/www/html
-HTTPD_CONF_DIR    = /etc/httpd/conf.d
-ifeq ($(strip $(MAJOR_VERSION)),5)
-USE_MOD_PYTHON    = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),6)
-USE_MOD_WSGI      = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),7)
-USE_MOD_WSGI      = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
+    WWW_DOCUMENT_ROOT = /var/www/html
+    HTTPD_CONF_DIR    = /etc/httpd/conf.d
+    ifeq ($(strip $(MAJOR_VERSION)),5)
+        USE_MOD_PYTHON    = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),6)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python-config
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),7)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+        ifeq ($(shell test $(MINOR_VERSION) -gt 6; echo $$?),0)
+            USE_PYTHON2      = 0
+            USE_PYTHON3      = 1
+            PYTHON_EXEC_FILE = python3
+            PYTHON_CONFIG    = python3-config
+        endif
+    endif
 endif
 
 ########## Scientific Linux CERN ##########
 ifeq ($(strip $(OS)),ScientificCERNSLC)
-WWW_DOCUMENT_ROOT = /var/www/html
-HTTPD_CONF_DIR    = /etc/httpd/conf.d
-ifeq ($(strip $(MAJOR_VERSION)),5)
-USE_MOD_PYTHON    = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),6)
-USE_MOD_WSGI      = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
+    WWW_DOCUMENT_ROOT = /var/www/html
+    HTTPD_CONF_DIR    = /etc/httpd/conf.d
+    ifeq ($(strip $(MAJOR_VERSION)),5)
+        USE_MOD_PYTHON    = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),6)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+    endif
 endif
 # CERN CentOS 7 is like a CentOS 7
 
@@ -72,30 +79,37 @@ endif
 ifeq ($(strip $(OS)),CentOS)
 WWW_DOCUMENT_ROOT = /var/www/html
 HTTPD_CONF_DIR    = /etc/httpd/conf.d
-ifeq ($(strip $(MAJOR_VERSION)),5)
-USE_MOD_PYTHON    = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),6)
-USE_MOD_WSGI      = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),7)
-USE_MOD_WSGI      = 1
-USE_PYTHON2       = 1
-PYTHON_EXEC_FILE  = python2
-PYTHON_CONFIG     = python2-config
-endif
-ifeq ($(strip $(MAJOR_VERSION)),8)
-USE_MOD_WSGI      = 1
-USE_PYTHON3       = 1
-PYTHON_EXEC_FILE  = python3
-PYTHON_CONFIG     = python3-config
-endif
+    ifeq ($(strip $(MAJOR_VERSION)),5)
+        USE_MOD_PYTHON    = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),6)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python-config
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),7)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON2       = 1
+        PYTHON_EXEC_FILE  = python2
+        PYTHON_CONFIG     = python2-config
+        # Use python3 on CentOS 7.7 and later
+        ifeq ($(shell test $(MINOR_VERSION) -gt 6; echo $$?),0)
+            USE_PYTHON2 = 0
+            USE_PYTHON3 = 1
+            PYTHON_EXEC_FILE  = python3
+            PYTHON_CONFIG     = python3-config
+        endif
+    endif
+    ifeq ($(strip $(MAJOR_VERSION)),8)
+        USE_MOD_WSGI      = 1
+        USE_PYTHON3       = 1
+        PYTHON_EXEC_FILE  = python3
+        PYTHON_CONFIG     = python3-config
+    endif
 endif
 
 ########## Fedora ##########
@@ -167,10 +181,10 @@ endif
 
 # all target for testing #
 #all:
-#	@echo "os:" $(OS)
-#	@echo "version:" $(VERSION)
-#	@echo "arch:" $(ARCH)
-#	@echo "major_version:" $(MAJOR_VERSION)
-#	@echo "httpd_document_root:" $(HTTPD_DOCUMENT_ROOT)
-#	@echo "mod_python:" $(USE_MOD_PYTHON)
-#	@echo "mod_wsgi:" $(USE_MOD_WSGI)
+#       @echo "os:" $(OS)
+#       @echo "version:" $(VERSION)
+#       @echo "arch:" $(ARCH)
+#       @echo "major_version:" $(MAJOR_VERSION)
+#       @echo "httpd_document_root:" $(HTTPD_DOCUMENT_ROOT)
+#       @echo "mod_python:" $(USE_MOD_PYTHON)
+#       @echo "mod_wsgi:" $(USE_MOD_WSGI)
